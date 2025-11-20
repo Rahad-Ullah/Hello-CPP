188378f29b9c0252d068d1805c0b825d9006091a
Rahad Ullah <rahadullah10@gmail.com>
2025-11-20 20:48:49 +0600

    built-in swap function

--- min_max_swap.cpp
+++ min_max_swap.cpp
@@ -6,17 +6,25 @@ int main()
     int a, b;
     cin >> a >> b;
 
-    if (a < b)
-    {
-        cout << a << endl;
-    }
-    else
-    {
-        cout << b << endl;
-    }
+    // if (a < b)
+    // {
+    //     cout << a << endl;
+    // }
+    // else
+    // {
+    //     cout << b << endl;
+    // }
 
     cout << min({a, b, 3, 7, 13}) << endl;
     cout << max({a, b, 20}) << endl;
+
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    swap(a, b);
+
+    cout << a << " " << b << endl;
 
     return 0;
 }
