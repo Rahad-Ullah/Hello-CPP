bdfb0e338f87ba211629f4d8cadf52e4720f5680
Rahad Ullah <rahadullah10@gmail.com>
2025-11-20 19:30:13 +0600

    introduction in c++

---
+++ print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+   std::cout << "Hello World" << std::endl;
+
+   int a = 10;
+   std::cout << a << std::endl;
+
+   char c = 'A';
+   std::cout << c << std::endl;
+
+   std::cout << a << std::endl
+             << c << std::endl;
+
+   return 0;
+}
